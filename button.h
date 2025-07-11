97d0f589c5f53cf4566ddbca001254897468dcfc
a.savanovich <asavanster@gmail.com>
2025-07-11 06:20:52 +0300

    style

--- button.h
+++ button.h
@@ -1,40 +1,38 @@
-#include <vector>
 #include <Windows.h>
+#include <vector>
 class field;
-class button
-{
-	int i, j;
-	void paint_zero();
-	void paint_cross();
-	void InitWindow();
-	bool is_pushed;
-	size_t BN_ID;
-	field * parent;
-	int symbol;
-	HWND hW; // comment if use in linux
-public:
-	int  get_symbol () const {return symbol;}
-	bool push(bool is_first);
-	button(int _j, int _i, field *_parent);
+class button {
+    int i, j;
+    void paint_zero();
+    void paint_cross();
+    void InitWindow();
+    bool is_pushed;
+    size_t BN_ID;
+    field* parent;
+    int symbol;
+    HWND hW; // comment if use in linux
+  public:
+    int get_symbol() const { return symbol; }
+    bool push(bool is_first);
+    button(int _j, int _i, field* _parent);
 };
 
-class field
-{
-	bool is_first;
-	int _size;
-	//HWND hWf;
-	std::vector< std::vector<button> > arr;
-	bool ChekState(size_t j, size_t i) const;
-	//friend check;check Check;
-	bool Go(int chek_symbol, size_t i, size_t j, bool (**change_i_j)(size_t &i, size_t &j, size_t size) ) const;
-public:
-	bool is_cross () {return is_first;}
-	field(int field_size);
-	void push(size_t ID);
-	int size() const {return _size;}
-};
+class field {
+    bool is_first;
+    int _size;
+    // HWND hWf;
+    std::vector<std::vector<button>> arr;
+    bool ChekState(size_t j, size_t i) const;
+    // friend check;check Check;
+    bool Go(int chek_symbol, size_t i, size_t j,
+            bool (**change_i_j)(size_t& i, size_t& j, size_t size)) const;
 
+  public:
+    bool is_cross() { return is_first; }
+    field(int field_size);
+    void push(size_t ID);
+    int size() const { return _size; }
+};
 
 void Victory(bool is_first);
-size_t get_bn_id(size_t j,size_t i);
-
+size_t get_bn_id(size_t j, size_t i);
